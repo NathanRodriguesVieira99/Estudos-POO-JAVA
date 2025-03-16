f21c621d9c1acfe587272719fa4faf99a82f4438
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-16 12:19:22 -0300

    feat: create my first class with  attributes and methods

---
+++ src/Ninja.java
@@ -0,0 +1,13 @@
+// crio a classe (que é basicamente um molde para criação)
+public class Ninja {
+    // atributos da classe
+    String nome;
+    String sensei;
+    String aldeia;
+    String melhorTecnica;
+
+    // metodo(s) da classe (são ações que o objeto pode ter)
+    void usarTecnica() {
+        System.out.println(nome + " usou a tecnica: " + melhorTecnica);
+    };
+}
